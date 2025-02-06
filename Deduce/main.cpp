1b9604168bc1f149646b55af7dd9be5577ca7556
Gilles Maire <gilles@gillesmaire.com>
2025-02-06 14:26:51 +0100

    error corrected

--- Deduce/main.cpp
+++ Deduce/main.cpp
@@ -3,22 +3,31 @@
 //using namespace chess;
 
 
-std::string deduceMove(chess::Board board, std::string shortmove)
+std::string deduceMove(chess::Board &board, std::string shortmove)
 {
   chess::Movelist moves;
   chess::movegen::legalmoves(moves, board);
   for (const auto &move : moves) {
         std::string m=chess::uci::moveToUci(move);
         std::string m2=chess::uci::moveToSan(board, move);
-        if ( m2==shortmove) return m;
-    }
+        if ( m2==shortmove)  {
+          board.makeMove(move);
+          return m;
+        }
+	}
   return std::string("error");
 }
 
 int main()
 {
+
     chess::Board b(chess::constants::STARTPOS);
+    std::cout<<b<<std::endl;
     std::string m=deduceMove(b,"e4");
     std::cout<<m<<std::endl;
-  
+    std::cout<<b<<std::endl;
+    m=deduceMove(b,"e5");
+    std::cout<<m<<std::endl;
+    std::cout<<b<<std::endl;
+
 }
